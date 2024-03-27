908fc78747041cbee448149976cfb630207c2d77
asimkoul <ahmadasim080gmail.com>
2024-03-27 17:47:25 +0530

    fetchdetails

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function handleFormSubmit(event){
-    
+ 
     event.preventDefault();
-    
+    // save details   
     let userDetails={
       username:event.target.username.value,
       email:event.target.email.value,
       phone:event.target.phone.value
     }
     localStorage.setItem(userDetails.username, JSON.stringify(userDetails))
-    axios.post("https://crudcrud.com/api/c933bc46402446aaa80dc97b4818fecc/appointmentData",userDetails)
+    axios.post("https://crudcrud.com/api/5613b7d227f4483f9725dafd8cb6ec52/appointmentData",userDetails)
     .then((res)=>console.log(res))
     .catch((err)=>console.log(err))
     showUserOnScreen(userDetails)
@@ -32,12 +32,23 @@ function handleFormSubmit(event){
         localStorage.removeItem(obj.username)
     })
     editBtn.addEventListener("click",function(){
-        userList.removeChild(userItem)
-        localStorage.removeItem(obj.username)
+    userList.removeChild(userItem)
+    localStorage.removeItem(obj.username)
 
-            document.getElementById("username").value=obj.username
-            document.getElementById("email").value=obj.email
-            document.getElementById("phone").value=obj.phone
+    document.getElementById("username").value=obj.username
+    document.getElementById("email").value=obj.email
+    document.getElementById("phone").value=obj.phone
 
     })
   }
+  // fetch details on refresh
+  window.addEventListener("DOMContentLoaded",()=>{
+    axios.get("https://crudcrud.com/api/5613b7d227f4483f9725dafd8cb6ec52/appointmentData")
+    .then((res)=>{
+      console.log(res)
+      for(let i=0;i<res.data.length;i++){
+        showUserOnScreen(res.data[i]);
+      }
+    })
+    .catch((err)=>console.log(err))
+  })
